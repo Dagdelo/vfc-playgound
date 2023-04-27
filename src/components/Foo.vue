58a5e8c9de941e8e531436b1012a9556eb45ddc2
Henrique Sales <henriquedelgadosales@gmail.com>
2023-04-27 19:25:44 -0300

    Basic usage

---
+++ src/components/Foo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This is Foo Component!</div>
+</template>
+<script lang="ts">
+//class component
+import { Component, Vue } from "vue-facing-decorator";
+@Component
+export default class Foo extends Vue {
+    speak() {
+        alert("Alert from Foo!")
+    }
+}
+</script>
+<style></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,51 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div @click="count = 2">Count: {{ count }}</div>
+  <input v-model="text">
+  <Foo @click="callMethodFromOutside()" ref="fooComp"/>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script lang="ts">
+import { Component, Ref, Vue } from "vue-facing-decorator";
+
+import Foo from './Foo.vue'
+// import { mapState, mapMutations } from 'vuex'
+
+@Component({
+  'components': {
+    Foo
+  }
+})
+export default class HelloWorld extends Vue {
+  //This is a vue reactive property.
+  text = "Example code";
+
+  //This is a vue component method.
+  method() {
+    console.log(this.$store.state.count)
+  }
+
+  // Reference
+  @Ref
+  readonly fooComp!: Foo
+
+  callMethodFromOutside() {
+    console.log(this.fooComp.speak())
+  }
+
+  //This is a vue component lifecycle hook.
+  mounted() {
+    this.method();
+  }
+
+  // Mapping vuex to class get and setter
+  get count() {
+    return this.$store.state.count;
+  }
+
+  set count(amount) {
+    this.$store.commit('increment', amount)
+  }
 }
-</style>
+</script>
+
+<style></style>
